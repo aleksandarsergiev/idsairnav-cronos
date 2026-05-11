23cb02a45900b2ac8ca28be246d72b594b3cad1d
Aleksandar Sergiev <aleksandar.sergiev@dreamix.eu>
2026-05-11 15:03:04 +0300

    Use absolute URL in SessionClient instead of relying on baseURL

--- tests/api/clients/SessionClient.ts
+++ tests/api/clients/SessionClient.ts
@@ -4,7 +4,7 @@ export class SessionClient {
   constructor(private request: APIRequestContext) {}
 
   async login(userLogin: string, password: string): Promise<APIResponse> {
-    return this.request.post('/session/login', {
+    return this.request.post(`${process.env.API_BASE_URL}/session/login`, {
       params: { userLogin, password, policies: true },
     });
   }
