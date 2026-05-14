5a9c8a8fe0a7b9d1b0db64d15d3a5ef667d09d9d
Aleksandar Sergiev <aleksandar.sergiev@dreamix.eu>
2026-05-14 11:07:00 +0300

    Assert localizedStatusDescription on user creation failure

--- tests/features/api/regression/createUser.feature
+++ tests/features/api/regression/createUser.feature
@@ -4,3 +4,6 @@ Feature: User API
   Scenario: Cannot create user linked to a non-existent child FPL office
     When I create a user linked to a non-existent child FPL office
     Then the user creation should fail because the child FPL office does not exist
+    And the user response should contain:
+      | field                      | value              |
+      | localizedStatusDescription | Resource not found |
--- tests/steps/api/createUser.steps.ts
+++ tests/steps/api/createUser.steps.ts
@@ -1,7 +1,7 @@
-import { createBdd } from 'playwright-bdd';
+import { createBdd, DataTable } from 'playwright-bdd';
 import { test } from '../../support/fixtures';
 import { userWithMissingChildFplOfficePayload } from '../../api/data/user';
-import { expectResponseStatus } from '../../api/assertions/responseAssertions';
+import { expectResponseStatus, expectResponseToContain } from '../../api/assertions/responseAssertions';
 
 const { When, Then } = createBdd(test);
 
@@ -10,6 +10,9 @@ When('I create a user linked to a non-existent child FPL office', async ({ userC
 });
 
 Then('the user creation should fail because the child FPL office does not exist', async ({ apiContext }) => {
-  console.log(await apiContext.response!.json());
-  expectResponseStatus(apiContext.response!, 417);
+  expectResponseStatus(apiContext.response!, 404);
+});
+
+Then('the user response should contain:', async ({ apiContext }, dataTable: DataTable) => {
+  await expectResponseToContain(apiContext.response!, dataTable);
 });
